b683a8c1a453737dbd105bd27633533703d08eb5
MagoDopado <eusebi.cirano@gmail.com>
2017-06-06 18:26:13 -0300

    Added reentrega 2

---
+++ entregas/entrega2/Reentrega Aparicio Eusebi/tp2-ej2.c
@@ -0,0 +1,223 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <math.h>
+#include <sys/time.h>
+
+/* Globals definition*/
+long i, N, paralelism;
+long double timetick;
+long *vector;
+long debug = 0, power;
+
+struct My_threads {
+	long init;
+	long fin;
+	pthread_t thread;
+	int id;
+};
+struct My_threads *p_threads;
+
+// ---------------------------------------------
+
+typedef struct {
+	pthread_mutex_t count_lock;
+	pthread_cond_t ok_to_proceed;
+	int count;
+} mylib_barrier_t;
+mylib_barrier_t *barriers;
+
+
+void mylib_init_barrier (mylib_barrier_t* b) {
+	b->count = 0;
+	pthread_mutex_init(&(b->count_lock), NULL);
+	pthread_cond_init(&(b->ok_to_proceed), NULL);
+}
+
+void mylib_barrier (mylib_barrier_t *b, int num_threads) {
+	pthread_mutex_lock(&(b->count_lock));
+	b->count++;
+	if (b->count == num_threads) {
+		b->count = 0;
+		pthread_cond_broadcast(&(b->ok_to_proceed));
+	} else {
+		pthread_cond_wait(&(b->ok_to_proceed),&(b->count_lock));
+	}
+	pthread_mutex_unlock(&(b->count_lock));
+}
+
+// ----------------------------------------
+
+/* Functions definition*/
+long double dwalltime() {
+	long double sec;
+	struct timeval tv;
+
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+
+void printVector(long* vector) {
+	for (long i = 0; i < N; i++) {
+			printf("%ld, ", vector[i]);
+	}
+	printf("\n");
+}
+
+#include <stdio.h>
+
+void qs(long *vector,long limite_izq,long limite_der)
+{
+    int izq, der, temporal, pivote;
+    izq = limite_izq;
+    der = limite_der;
+		if (der == izq) {
+			return;
+		}
+    pivote = vector[(izq+der)/2];
+
+    do {
+        while(vector[izq] < pivote && izq < limite_der) izq++;
+        while(pivote < vector[der] && der > limite_izq) der--;
+        if(izq <= der)
+        {
+            temporal = vector[izq];
+            vector[izq] = vector[der];
+            vector[der] = temporal;
+            izq++;
+            der--;
+        }
+
+    } while(izq <= der);
+    if(limite_izq < der) qs(vector, limite_izq, der);
+    if(limite_der > izq) qs(vector, izq, limite_der);
+}
+
+void quickSort(long *vector, long inicio, long fin) {
+		qs(vector, inicio, fin);
+}
+
+void merge(long init, long fin) {
+	long N = (fin - init + 1);
+	long *vectorTemp = (long*)malloc(sizeof(long)*(N));
+	long actLeft = init;
+	long actRight = init + ( N / 2);
+	for(long i=0;i<N;i++){
+		if (actLeft == init + ( N / 2)) {
+			vectorTemp[i] = vector[actRight];
+			actRight++;
+		}
+		else {
+			if (actRight == fin+1) {
+				vectorTemp[i] = vector[actLeft];
+				actLeft++;
+			} else {
+				if (vector[actLeft] < vector[actRight]) {
+					vectorTemp[i] = vector[actLeft];
+					actLeft++;
+				} else {
+					vectorTemp[i] = vector[actRight];
+					actRight++;
+				}
+			}
+		}
+	}
+	for(long i=0;i<N;i++) vector[init+i] = vectorTemp[i];
+	free (vectorTemp);
+}
+
+void *ordenar(void* args) {
+	int p_id = *((int *)args);
+	quickSort(vector, p_threads[p_id].init, p_threads[p_id].fin);
+
+	int cant = log2(paralelism);
+	if ((p_id%2) == 0) {
+
+		for (int i = 1; i <= cant; i++) {
+			if (p_id%(2*i) == 0) {
+
+				int nextMergeId = (int)(p_id+(pow(2, i-1)));
+				//printf("p_id: %d\twait thread: %d\n",p_id,nextMergeId);
+				// pthread_join(( p_threads[(int)(p_id+(pow(2, i-1)))].thread ), NULL);
+				mylib_barrier(&barriers[nextMergeId], 2);
+
+				p_threads[p_id].fin = p_threads[nextMergeId].fin;
+				//printf("p_id: %d -\tthread %d: finish\tfin: %ld\n",p_id, nextMergeId, p_threads[p_id].fin);
+				merge(p_threads[p_id].init, p_threads[p_id].fin);
+			} else {
+				//printf("p_id: %d\tIm wait\n",p_id);
+				mylib_barrier(&barriers[p_id], 2);
+			}
+		}
+	} else {
+		//printf("p_id: %d\tIm wait\n",p_id);
+		mylib_barrier(&barriers[p_id], 2);
+	}
+}
+
+int main(long argc, char*argv[]) {
+
+	/* Argument validation.*/
+	const char* help ="\nCompilar:\n\tgcc -o tp2-ej2 tp2-ej2.c -lm -std=c99 -pthread\nArgumentos:\n\t<E> = 2^E elementos del vector.\n\t<P> = Paralelismo a utilizar.\n\t<S> = Cantidad de muestras a ejecutar.";
+
+	if (argc < 4) {
+		printf("%s\n", help);
+		return -1;
+	}
+	if (argc > 4) {
+		debug = atoi(argv[4]);
+	}
+
+	/* Globals initialization.*/
+	power = atoi(argv[1]);
+  N = pow(2, power);
+	paralelism = atoi(argv[2]);
+	p_threads = malloc(sizeof(struct My_threads) * paralelism);
+	barriers = malloc(sizeof(mylib_barrier_t) * (paralelism));
+
+	pthread_attr_t attr;
+	pthread_attr_init (&attr);
+
+	printf("CANTIDAD DE THREADS: %ld.\n", paralelism);
+	printf("N: %ld.\n", N);
+
+	vector=(long*)malloc(sizeof(long)*N);
+
+	srand(time(NULL));
+	for(i=0;i<N;i++){
+		vector[i] = rand();
+	}
+
+	if (debug) {
+		printVector(vector);
+	}
+
+	timetick = dwalltime();
+
+	long cantThreads = paralelism;
+  for(i = 0; i < paralelism; i++){
+		mylib_init_barrier(&barriers[i]);
+
+		p_threads[i].id = i;
+		p_threads[i].init = N/paralelism * i;
+		p_threads[i].fin = (N/paralelism * (i+1))-1;
+	}
+	for(i=0; i < paralelism; i++) {
+		pthread_create(&p_threads[i].thread, NULL, ordenar, &p_threads[i].id);
+	}
+
+	pthread_join(p_threads[0].thread, NULL);
+	long double tiempo = dwalltime() - timetick;
+
+	for(i=1; i < paralelism; i++) {
+		pthread_join(p_threads[i].thread, NULL);
+	}
+	printf("Tiempo en segundos %LF \n", tiempo);
+
+
+	if (debug) {
+		printVector(vector);
+	}
+
+}
---
+++ entregas/entrega2/Reentrega Aparicio Eusebi/tp2-ej3.c
@@ -0,0 +1,103 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+
+/* Globals definition*/
+size_t exponent = 24;
+size_t edge;
+size_t size;
+size_t paralelism = 1;
+size_t iterations = 10;
+long double totalTime = 0.0;
+size_t debug = 0;
+
+long double dwalltime() {
+	long double sec;
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+
+size_t* initMatrix() {
+	size_t element = 0;
+	size_t* matrix = calloc(size, sizeof(size_t));
+	for (size_t rows = 0; rows < edge; rows++) {
+		size_t yOffset = rows * edge;
+		for (size_t columns = 0; columns <= rows; columns++) {
+			size_t totalOffset = yOffset + columns;
+			matrix[totalOffset] = ++element;
+		}
+	}
+	return matrix;
+}
+
+void printMatrix(size_t* matrix) {
+	for (size_t rows = 0; rows < edge; rows++) {
+		size_t yOffset = rows * edge;
+		for (size_t columns = 0; columns < edge; columns++) {
+			size_t totalOffset = yOffset + columns;
+			printf("%zu, ", matrix[totalOffset]);
+		}
+		printf("\n");
+	}
+}
+
+int main(int argc, char const *argv[]) {
+
+	/* Argument validation.*/
+	const char* help ="\nCompilar:\n\tgcc -o ej3 ej3.c -lm -std=c99 -fopenmp\nArgumentos:\n\t<E> = 2^E elementos por lado de la matriz.\n\t<P> = Paralelismo a utilizar.\n\t<S> = Cantidad de muestras a ejecutar.";
+
+	if (argc < 4) {
+		printf("%s\n", help);
+		return -1;
+	}
+
+	/* Globals initialization.*/
+	exponent = atoi(argv[1]);
+	edge = pow(2, exponent);
+	size = edge * edge;
+
+	paralelism = atoi(argv[2]);
+	omp_set_num_threads(paralelism);
+
+	iterations = atoi(argv[3]);
+	if (argc > 4) {
+		debug = atoi(argv[4]);
+	}
+
+	/* Main loop.*/
+	for (size_t round = 0; round < iterations; round++) {
+		size_t* matrix = initMatrix();
+		long double start = dwalltime();
+		/* Start race.*/
+		#pragma omp parallel for schedule(dynamic, 1)
+		//#pragma omp parallel for schedule(guided)
+		for (size_t rows = edge - 1; rows > 0; rows--) { // Backwards
+		//for (size_t rows = 0; rows < edge; rows++) { // Forward
+			size_t yOffset = rows * edge;
+			for (size_t columns = 0; columns <= rows; columns++) {
+				size_t originalOffset = yOffset + columns;
+				size_t transposedOffset = (columns * edge) + rows;
+				if (originalOffset != transposedOffset) {
+					matrix[transposedOffset] = matrix[originalOffset];
+					matrix[originalOffset] = 0;
+				}
+			}
+		}
+		/* End race.*/
+		long double end = dwalltime();
+		long double delta = end - start;
+		totalTime+= delta;
+
+		if (debug) {
+			printMatrix(matrix);
+		}
+		free(matrix);
+	}
+	long double average = totalTime / iterations;
+	printf("Average for edge of 2^%zu with %zu processors: %LF seconds.\n", exponent, paralelism, average);
+	return 0;
+}
