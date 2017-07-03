6e12eb20204a6d3b9f0006f997f1280dda1e45ef
Natii Aparicio <nataliaeaparicio@gmail.com>
2017-07-02 22:32:20 -0300

    base ej3

---
+++ entregas/entrega3/ejericio3.c
@@ -0,0 +1,39 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+#ifndef ROOT
+#define ROOT 0
+#endif
+#ifndef WORLD
+#define WORLD MPI_COMM_WORLD
+#endif
+
+// World globals.
+int paralelism, id, debug;
+const char* help ="\nCompilar en Linux Openmpi:\n\tmpicc -o 1 1.c -lm\nEjecutar en Openmpi:\n\tEn una sola maquina:\n\t\tmpirun -np <P> ejecutable <E>\n\t\t<P> = cantidad de procesos\n\t\t<E> = 2^E elementos del vector\n\tEn un cluster de máquinas:\n\t\tmpirun -np cantidadDeProcesos -machinefile archivoMaquinas ejecutable";
+
+// Data globals.
+long long exponent, size, scatteredEdge;
+
+// Id functions.
+int isRoot() {
+	return id == ROOT;
+}
+
+
+double stopwatch() {
+	double sec;
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	sec = tv.tv_sec + tv.tv_usec/1000000.0;
+	return sec;
+}
+
+
+int main(int argc, const *argv[]) {
+  /* code */
+  return 0;
+}
