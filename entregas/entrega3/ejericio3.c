f5b3e23fccc640f2d0436ed66674c57dcf4deb85
Natii Aparicio <nataliaeaparicio@gmail.com>
2017-07-03 00:12:53 -0300

    base ej3

--- entregas/entrega3/ejericio3.c
+++ entregas/entrega3/ejericio3.c
@@ -16,13 +16,61 @@ int paralelism, id, debug;
 const char* help ="\nCompilar en Linux Openmpi:\n\tmpicc -o 1 1.c -lm\nEjecutar en Openmpi:\n\tEn una sola maquina:\n\t\tmpirun -np <P> ejecutable <E>\n\t\t<P> = cantidad de procesos\n\t\t<E> = 2^E elementos del vector\n\tEn un cluster de máquinas:\n\t\tmpirun -np cantidadDeProcesos -machinefile archivoMaquinas ejecutable";
 
 // Data globals.
-long long exponent, size, scatteredEdge;
+long long exponent, size, scatteredSize;
+struct Map {
+  int num;
+  long occu;
+};
+
+struct Map *occurrences;
 
 // Id functions.
 int isRoot() {
 	return id == ROOT;
 }
 
+// Debug functions.
+int shouldPrint() {
+	return debug && isRoot();
+}
+
+void printArray(int* arr){
+  for (size_t i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int* newArray(long long size) {
+  return malloc(size * sizeof(int));
+}
+
+void freeArray(int* array) {
+	free(array);
+}
+
+int* initArray(long long size){
+  int* array = newArray(size);
+  // srand(time(NULL));
+  for (size_t i = 0; i < size; i++) {
+    array[i] = rand() % size/2;
+  }
+
+  return array;
+}
+
+int* createArray() {
+  int* array;
+  if (isRoot()) {
+    array = initArray(size);
+  } else {
+    array = newArray(scatteredSize);
+  }
+  MPI_Scatter(array, scatteredSize, MPI_INT,
+              array, scatteredSize, MPI_INT,
+              ROOT,WORLD);
+  return array;
+}
 
 double stopwatch() {
 	double sec;
@@ -33,7 +81,39 @@ double stopwatch() {
 }
 
 
-int main(int argc, const *argv[]) {
-  /* code */
+int main(int argc, char *argv[]) {
+
+  MPI_Init(&argc, &argv);
+	MPI_Comm_size(WORLD, &paralelism);
+	MPI_Comm_rank(WORLD, &id);
+
+  if (argc < 2) {
+    if (isRoot()) {
+      printf("%s\n", help);
+    }
+    MPI_Finalize();
+    return 0;
+  }
+
+  /* Data Globals initialization.*/
+	exponent = atoi(argv[1]);
+	size = pow(2, exponent);
+	scatteredSize = size / paralelism;
+	debug = 0;
+	if (argc > 2) {
+		debug = 1;
+	}
+	if (shouldPrint()) {
+		printf("exponent: %lld\nsize: %lld\n", exponent, size);
+	}
+
+  int* arr = createArray();
+
+  if (shouldPrint()) {
+		printf("result: \n");
+		printArray(arr);
+	}
+
+  MPI_Finalize();
   return 0;
 }
